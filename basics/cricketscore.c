611b7e87d7bd5162b9391935ef9a465dc7e52d93
anuragprasoon <anuragprasoon23@gmail.com>
2023-03-19 23:09:49 +0530

    cricket score project

---
+++ basics/cricketscore.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int run,wkt;
+    float rr,ovr;
+    run=121;
+    wkt=2;
+    rr=7.8;
+    ovr=15.2;
+    printf("Team A: %d/%d",run,wkt);
+    printf("\nOvers:%.1f RR:%.1f",ovr,rr);
+}
