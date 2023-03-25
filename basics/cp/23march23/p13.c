2fa6de00bafae4c9f9a07dd77324477abad75bfb
anuragprasoon <anuragprasoon23@gmail.com>
2023-03-26 01:14:52 +0530

    cp lab

---
+++ basics/cp/23march23/p13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+  int number, sum = 0;
+
+  // the body of the loop is executed at least once
+  do {
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    sum += number;
+  }
+  while(number != 0);
+
+  printf("Sum = %d",sum);
+
+}
